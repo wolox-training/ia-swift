9cf29c853335b7cf35cbb2fde79c48bb9d7824e7
Nacho 2 <ignacio.abadie@wolox.com.ar>
2020-01-16 15:17:07 -0300

    moved button styling to didSet function

--- WBooks/Screens/LoginView.swift
+++ WBooks/Screens/LoginView.swift
@@ -11,16 +11,15 @@ import WolmoCore
 
 class LoginView: UIView, NibLoadable {
 
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton! {
+        didSet {
+            loginButton.layer.borderWidth = 2.0
+            loginButton.layer.cornerRadius = 20
+            loginButton.layer.borderColor = UIColor.white.cgColor
+        }
+    }
     
     @IBAction func loginButtonAction(_ sender: UIButton) {
-        print("anda :D")
-    }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        loginButton.layer.borderWidth = 2.0
-        loginButton.layer.cornerRadius = 20
-        loginButton.layer.borderColor = UIColor.white.cgColor
+        print("Login Pressed")
     }
 }

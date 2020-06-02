5cb4bb2879992f93c9c36cf636cef9c4e3c81f97
Nacho 2 <ignacio.abadie@wolox.com.ar>
2020-06-02 14:47:31 -0300

    Added image picker

--- WBooks/Screens/Suggestions/SuggestionsViewController.swift
+++ WBooks/Screens/Suggestions/SuggestionsViewController.swift
@@ -34,9 +34,16 @@ class SuggestionsViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewSetup()
+    }
+}
+
+extension SuggestionsViewController {
+    func viewSetup() {
         view.layer.backgroundColor = UIColor.creamBlue?.cgColor
         setTitle(headerTitle: "SUGGEST_VIEW_HEADER_TITLE".localized())
         _view.setupSubmitButton(enabled: false)
+        self._view.updateSuggestionLoading(isLoading: false)
         _suggestionsViewModel.sugestState.signal.observeValues(setupSuggestStatus)
         _suggestionsViewModel.bindFormInputs(bookName: _view.bookName!,
                                              bookAuthor: _view.bookAuthor!,
@@ -44,6 +51,9 @@ class SuggestionsViewController: BaseViewController {
                                              bookGenre: _view.bookGenre!)
         _suggestionsViewModel.isFormComplete.signal.observeValues { valid in
             self._view.setupSubmitButton(enabled: valid)
+        }
+        _view.bookImage.reactive.controlEvents(.touchUpInside).observeValues { _ in
+            self.choseImage()
         }
         _view.submitButton.reactive.controlEvents(.touchUpInside).observeValues { _ in
             self._suggestionsViewModel.suggestBook()
@@ -63,5 +73,41 @@ class SuggestionsViewController: BaseViewController {
         case .loading:
             self._view.updateSuggestionLoading(isLoading: true)
         }
+    }
+}
+
+extension SuggestionsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func choseImage() {
+        let alertController = UIAlertController(title: .none, message: .none, preferredStyle: .actionSheet)
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let takePhotoAction = UIAlertAction(title: "Camera", style: .default) { _ in
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: .none)
+            }
+            alertController.addAction(takePhotoAction)
+        }
+
+        let chooseGalleryAction = UIAlertAction(title: "Gallery", style: .default) { _ in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: .none)
+        }
+        alertController.addAction(chooseGalleryAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: .none)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            _view.bookImage.setImage(image, for: .normal)
+        }
+                picker.dismiss(animated: true, completion: nil)
     }
 }
